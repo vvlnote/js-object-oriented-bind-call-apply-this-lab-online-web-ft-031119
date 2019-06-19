101e215c8d1b911f426ae25b11bf7f69a15ff4f8
Joy Lin <vvlnote@hotmail.com>
2019-06-19 17:05:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg);
 }
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
+}
