c095cdf671a65f3101dc7edd390e563b48f8f289
Joy Lin <vvlnote@hotmail.com>
2019-06-19 17:07:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args);
 }
+
+function returnNewFunctionOf(functionToBeCopies, thisValue) {
+  
+}
